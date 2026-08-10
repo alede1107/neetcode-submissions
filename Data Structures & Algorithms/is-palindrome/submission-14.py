76f951d7e0429423bd5f732906664a03127a71bb
Alejandro Perdomo <157928073+alede1107@users.noreply.github.com>
2026-08-09 21:44:17 -0400

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+
+        for char in s.lower():
+            if char.isalpha() or char.isdigit():
+                res+= char
+        l, r = 0, len(res) - 1
+
+        while l <= r:
+            if res[l] != res[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
